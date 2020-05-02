d361bd4e821b02cf800fab5e4e685bec85d648a3
frankkulak <fs.kulak@gmail.com>
2020-05-01 20:32:01 -0400

    Automate experience span with JS.

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,73 @@
-// Object of experiences and their start/end dates
+// --------------------------------------------------------------------------------
+// Experience duration
+
+// Experience IDs and their start/end dates
 // IMPORTANT: Months are off by 1 (Jan = 0, Feb = 1, etc.)
 const experiences = {
     // co-ops and internships
-    'expdur-tjx': {
+    'tjx': {
         'start': new Date(2019, 6), // Jul 2019
         'end': new Date(2019, 11) // Dec 2019
     },
-    'expdur-mah': {
+    'mah': {
         'start': new Date(2016, 6), // Jul 2016
         'end': new Date(2016, 7) // Aug 2016
     },
     // part-time work
-    'expdur-tutoring': {
+    'nututor': {
         'start': new Date(2019, 0), // Jan 2019
         'end': new Date() // Present
     },
-    'expdur-sherwood': {
+    'sherwood': {
         'start': new Date(2018, 5), // Jun 2018
         'end': new Date(2019, 6) // Jul 2019
     },
-    'expdur-sfs': {
+    'sfs': {
         'start': new Date(2017, 8), // Sep 2017
         'end': new Date(2020, 2) // Mar 2020
     },
-    'expdur-barnes': {
+    'barnes': {
         'start': new Date(2017, 5), // Jun 2017
         'end': new Date(2017, 7) // Aug 2017
     },
     // volunteer work
-    'expdur-berentlab': {
+    'berlab': {
         'start': new Date(2018, 0), // Jan 2018
         'end': new Date(2018, 11) // Dec 2018
     },
-    'expdur-lgr': {
+    'lgr': {
         'start': new Date(2017, 9), // Oct 2017
         'end': new Date(2017, 10) // Nov 2017
     },
-    'expdur-mobilecsp': {
+    'csp': {
         'start': new Date(2016, 7), // Aug 2016
         'end': new Date(2017, 10) // Jun 2017
     },
-    'expdur-nhstutor': {
+    'hstutor': {
         'start': new Date(2014, 8), // Sep 2014
         'end': new Date(2017, 5) // Jun 2017
     },
-    'expdur-dischal': {
+    'dc': {
         'start': new Date(2011, 9), // Oct 2011
         'end': new Date(2016, 11) // Dec 2016
     },
 };
 
-// Returns a formatted string for the duration between two dates.
+// Returns a formatted string for the span between two dates (e.g. "Jan. 2019 - Dec. 2019").
+const experienceSpan = (startDate, endDate) => {
+    const months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+    const formatDate = date => `${months[date.getMonth()]} ${date.getFullYear()}`;
+    const present = new Date();
+
+    // formatting and return start and end dates
+    const formattedStart = startDate.getFullYear() === endDate.getFullYear() ?
+        months[startDate.getMonth()] : formatDate(startDate);
+    const formattedEnd = endDate.getMonth() === present.getMonth() && endDate.getFullYear() === present.getFullYear() ?
+        'Present' : formatDate(endDate);
+    return `${formattedStart} - ${formattedEnd}`;
+};
+
+// Returns a formatted string for the duration between two dates (e.g. "1 yr 6 mos").
 const experienceDuration = (startDate, endDate) => {
     // find number of full years between the dates
     let years = endDate.getFullYear() - startDate.getFullYear();
@@ -75,11 +92,15 @@ const experienceDuration = (startDate, endDate) => {
     }
 };
 
-// Sets the inner HTML of each experience duration to the calculated experience duration.
+// Sets the inner HTML of each experience span/duration element.
 Object.keys(experiences).forEach(id => {
-    const { start, end } = experiences[id];
-    document.getElementById(id).innerHTML = experienceDuration(start, end);
+    const {start, end} = experiences[id];
+    document.getElementById(`expspn-${id}`).innerHTML = experienceSpan(start, end);
+    document.getElementById(`expdur-${id}`).innerHTML = experienceDuration(start, end);
 });
+
+// --------------------------------------------------------------------------------
+// Auto-scrolling
 
 // Automatically scrolls to the top of a card when it is opened.
 $('.card').on('shown.bs.collapse', function () {
