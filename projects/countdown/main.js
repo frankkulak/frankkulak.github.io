a62978c9c1701dfa50330888ef069872d3d95583
frankkulak <fs.kulak@gmail.com>
2020-05-11 15:29:59 -0400

    Improve countdown.

--- projects/countdown/main.js
+++ projects/countdown/main.js
@@ -6,11 +6,17 @@ const totalTime = endDate.getTime() - startDate.getTime();
 
 // populating label text
 const timeRemaining = totalTime - timeSinceStart;
-const daysRemaining = (timeRemaining / 1000) / 86400;
-$('#progress-label').html(`Days remaining: ${Math.round(daysRemaining)}`);
+const days = (timeRemaining / 1000) / 86400;
+const wholeDays = Math.floor(days);
+const hours = (days - wholeDays) * 24;
+const wholeHours = Math.floor(hours);
+const minutes = (hours - wholeHours) * 60;
+const wholeMinutes = Math.floor(minutes);
+$('#progress-label').html(`${wholeDays}d ${wholeHours}h ${wholeMinutes}m`);
 
 // populating progress bar
-const progressString = `${Math.floor((timeSinceStart / totalTime) * 100)}%`;
+const percentComplete = Math.floor((timeSinceStart / totalTime) * 100);
+const progressString = `${percentComplete}%`;
 const progressBar = $('#progress-bar');
 progressBar.width(progressString);
 progressBar.html(progressString);
