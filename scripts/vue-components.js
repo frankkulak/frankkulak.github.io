8fe4149bd9f9a31449ffe8a45559d7d69d9d9ca0
frankkulak <fs.kulak@gmail.com>
2020-05-27 20:26:29 -0400

    Formatting.

--- scripts/vue-components.js
+++ scripts/vue-components.js
@@ -1,4 +1,8 @@
 Vue.component('section-header', {
     props: ['title'],
-    template: `<div class="section-header"><h1>{{ title }}</h1><hr></div>`
+    template: `
+        <div class="section-header">
+            <h1>{{ title }}</h1>
+            <hr>
+        </div>`
 });
