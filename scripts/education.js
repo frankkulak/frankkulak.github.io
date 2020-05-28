774a0c18ed13b604bd227e5984f9380ba75db98d
frankkulak <fs.kulak@gmail.com>
2020-05-27 22:00:03 -0400

    Touch up education.

--- scripts/education.js
+++ scripts/education.js
@@ -5,57 +5,76 @@ Vue.component('education-content', {
         }
     },
     template: `
-        <div>
-            <school-display v-for="school in schools" v-bind:school="school" v-bind:key="school.code"></school-display>
+        <div class="education-content">
+            <school-display v-for="school in schools"
+                            v-bind:school="school"
+                            v-bind:key="school.code"></school-display>
         </div>`
 });
 
+const showDetailsButtonText = {show: 'show details', hide: 'hide details'};
 Vue.component('school-display', {
     props: ['school'],
     data: function () {
         return {
             detailsShown: false,
-            buttonText: 'show details'
+            buttonText: showDetailsButtonText.show,
+            courseInProgress: false
         }
     },
     methods: {
         toggleDetails: function () {
             this.detailsShown = !this.detailsShown;
-            this.buttonText = this.detailsShown ? 'hide details' : 'show details'
+            this.buttonText = this.detailsShown ? showDetailsButtonText.hide : showDetailsButtonText.show
         },
-        formatCourses: function (courses) { // fixme replace with css
-            const formatCourse = ({title, code, number, ongoing}) => `${title} (${code} ${number})${ongoing ? '*' : ''}`;
-            let output = [];
-            courses.forEach(course => output.push(formatCourse(course)));
-            return output.join(', ');
+        formatCourse: function ({title, code, number, ongoing}) {
+            if (ongoing) this.courseInProgress = true;
+            return `${title} (${code} ${number})${ongoing ? '*' : ''}`
         }
     },
     template: `
-        <div class="row school-preview">
+        <div class="row justify-content-center school-display">
             <div class="col-12 col-md-10 school-header">
-                <h5>{{ school.name }}, <span class="location">{{ school.location }}</span></h5>
-                <p>{{ school.degree }} <span class="grad-date">({{ school.graduation }})</span></p>
+                <h5>{{school.name}}, <span class="location">{{school.location}}</span></h5>
+                <p>{{school.degree}} <span class="grad-date">({{school.graduation}})</span></p>
             </div>
             <div class="col-md-2 btn-col">
                 <div class="btn" @click="toggleDetails">{{ buttonText }}</div>
             </div>
             <transition name="details">
                 <div v-show="detailsShown" class="details">
-                    <p>GPA: {{ school.gpa }}/4.0</p>
-                    <h6>honors</h6>
-                    <p v-for="honor in school.honors" v-html="honor"></p>
-                    <h6>courses</h6>
                     <table>
+                        <tr class="title-row">
+                            <td>
+                                <h6>honors</h6>
+                            </td>
+                            <td class="gpa">
+                                GPA: {{ school.gpa }}/4.0
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <p v-for="honor in school.honors" v-bind:key="honor" v-html="honor"></p>
+                            </td>
+                        </tr>
+                        <tr class="title-row">
+                            <td>
+                                <h6>courses</h6>
+                            </td>
+                        </tr>
                         <tr v-for="subject in school.subjects">
                             <td class="category">
                                 {{ subject.name }}
                             </td>
                             <td>
-                                {{ formatCourses(subject.courses) }}
+                                <ul class="course-list">
+                                    <li v-for="course in subject.courses"
+                                        v-bind:key="course.title">{{formatCourse(course)}}</li>
+                                </ul>
                             </td>
                         </tr>
                     </table>
-                    <p>* = in progress</p>
+                    <p v-show="courseInProgress">* = in progress</p>
                 </div>
             </transition>
         </div>`
