55a2b3ce1ee9d9d5a43b3935840db1483bbf3909
frankkulak <fs.kulak@gmail.com>
2020-06-05 15:14:47 -0400

    Add decals to home.

--- scripts/components/home.js
+++ scripts/components/home.js
@@ -6,7 +6,10 @@
             }
         },
         template: `
-            <div>
+            <div class="home-content">
+                <img class="decal top-left" src="./images/home-decals/top-left.svg" alt="decal">
+                <img class="decal top-right" src="./images/home-decals/top-right.svg" alt="decal">
+                <img class="decal bottom-left" src="./images/home-decals/bottom-left.svg" alt="decal">
                 <h1>Hi, I'm <strong>Frank Kulak</strong>.<br>I'm a software developer.</h1>
                 <div class="btn" @click="handleClick($event)">learn more <i class="fas fa-arrow-down"></i></div>
             </div>`
