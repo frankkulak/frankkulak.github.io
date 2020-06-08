18231104bd228404295ea9ec1aa8a107c71162a2
frankkulak <fs.kulak@gmail.com>
2020-06-08 15:01:04 -0400

    Improve home animation.

--- scripts/components/home.js
+++ scripts/components/home.js
@@ -10,8 +10,10 @@
                 <img class="decal top-left" src="./images/home-decals/top-left.svg" alt="decal">
                 <img class="decal top-right" src="./images/home-decals/top-right.svg" alt="decal">
                 <img class="decal bottom-left" src="./images/home-decals/bottom-left.svg" alt="decal">
-                <h1>Hi, I'm <strong>Frank Kulak</strong>.<br>I'm a software developer.</h1>
-                <div class="btn" @click="handleClick($event)">learn more <i class="fas fa-arrow-down"></i></div>
+                <div class="home-text">
+                    <h1>Hi, I'm <strong>Frank Kulak</strong>.<br>I'm a software developer.</h1>
+                    <div class="btn" @click="handleClick($event)">learn more <i class="fas fa-arrow-down"></i></div>
+                </div>
             </div>`
     });
 })();
