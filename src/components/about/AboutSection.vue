3a4c9cb83e1c1ea3932937c13efce181ec583eab
Frank Kulak <fs.kulak@gmail.com>
2022-05-05 11:43:20 -0400

    Update AboutSection.vue

--- src/components/about/AboutSection.vue
+++ src/components/about/AboutSection.vue
@@ -8,8 +8,9 @@
                     <h3 class="my-4">Frank Kulak</h3>
                 </b-col>
                 <b-col cols="12" sm="10" md="7" class="my-auto">
-                    <p>I am a recent graduate of Northeastern University, where I earned a Bachelor of Science in
-                        Computer Science and Linguistics, and am currently working as a mobile app developer at TJX.</p>
+                    <p>I'm a full-time app &amp; web developer at The TJX Companies, and a freelance open-source
+                        contributor and Sims 4 mod creator. I'm passionate about programming and linguistics, and am
+                        always looking for opportunities to mix them together.</p>
                 </b-col>
             </b-row>
         </b-container>
@@ -17,21 +18,21 @@
 </template>
 
 <script>
-    import SectionHeader from "../SectionHeader";
+import SectionHeader from "../SectionHeader";
 
-    export default {
-        name: "AboutSection",
-        components: {SectionHeader}
-    }
+export default {
+    name: "AboutSection",
+    components: {SectionHeader}
+}
 </script>
 
 <style lang="scss">
-    section#about {
-        img {
-            min-width: 45%;
-            max-width: 196px;
-            height: auto;
-            border-radius: 50%;
-        }
+section#about {
+    img {
+        min-width: 45%;
+        max-width: 196px;
+        height: auto;
+        border-radius: 50%;
     }
+}
 </style>
