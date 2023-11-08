f37bc6a535d7dabd1ad439033149fdce60f3153d
frankkulak <fs.kulak@gmail.com>
2023-11-08 09:18:35 -0500

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,10 +4,10 @@
     <a href="https://svelte.dev/" target="_blank">Svelte</a>
     and
     <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>, hosted
-    on <a href="https://pages.github.com/" target="_blank">GitHub pages</a>. All
-    icons are sourced from
-    <a href="https://ionic.io/ionicons" target="_blank">Ionicons</a>. Website
-    layout and design are inspired by
+    on <a href="https://pages.github.com/" target="_blank">GitHub Pages</a>. All
+    icons sourced from
+    <a href="https://ionic.io/ionicons" target="_blank">Ionicons</a>. Layout and
+    design inspired by
     <a href="https://brittanychiang.com/" target="_blank">Brittany Chiang</a>.
   </p>
   <p class="text-subtle text-sm">2023 © Frank Kulak</p>
